d1c54db8a68333d25a4216937b568641067f36b1
Uzziel Gonzalez <uzzielgonzalez247@gmail.com>
2024-10-21 02:04:47 -0400

    Imported loading page

--- js/homepage.js
+++ js/homepage.js
@@ -6,9 +6,35 @@ let mainHTML = `  <div class="title"><img src="../imgs/Gabis-cup-game-title.png"
 
    `;
 
-document.querySelector('.main').innerHTML = mainHTML;
+const mainDiv = document.querySelector('.main');
+
+mainDiv.innerHTML = mainHTML;
 
 document.body.addEventListener('click', function (evt) {
   console.log(evt.target);
   console.log('clicked!');
+
+  mainDiv.innerHTML = `   <div class="scores">
+        <div class="game-current-score">Current score: 0.00s</div>
+        <div class="game-high-score">High score: 0.00s</div>
+      </div>
+
+      <div class="settings-cog"><img src="../imgs/Settings-cog.png" /></div>
+
+      <div class="table-group">
+        <div class="cups">
+          <img src="../imgs/white-cup.png" />
+          <img src="../imgs/white-cup.png" />
+          <img src="../imgs/white-cup.png" />
+          <img src="../imgs/white-cup.png" />
+          <img src="../imgs/white-cup.png" />
+          <img src="../imgs/white-cup.png" />
+          <img src="../imgs/white-cup.png" />
+        </div>
+        <div class="table"><img src="../imgs/table.png" /></div>
+      </div>
+
+      <div class="play">Shuffling Cups...</div>`;
 });
+
+let cupColors = [];
