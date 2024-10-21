daa3356327b7df0483302512e19076375bdb0cff
Uzziel Gonzalez <uzzielgonzalez247@gmail.com>
2024-10-21 03:15:20 -0400

    Shuffle Function added

--- js/homepage.js
+++ js/homepage.js
@@ -10,11 +10,38 @@ const mainDiv = document.querySelector('.main');
 
 mainDiv.innerHTML = mainHTML;
 
-document.body.addEventListener('click', function (evt) {
-  console.log(evt.target);
-  console.log('clicked!');
+let cupColors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
+let comparisonColors = [
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'purple',
+  'pink',
+];
 
-  mainDiv.innerHTML = `   <div class="scores">
+function shuffle(array) {
+  let currentIndex = array.length;
+
+  while (currentIndex != 0) {
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+}
+
+let clicked = false;
+
+document.body.addEventListener('click', function (evt) {
+  if (clicked === true) {
+    return;
+  } else {
+    mainDiv.innerHTML = `   <div class="scores">
         <div class="game-current-score">Current score: 0.00s</div>
         <div class="game-high-score">High score: 0.00s</div>
       </div>
@@ -22,19 +49,64 @@ document.body.addEventListener('click', function (evt) {
       <div class="settings-cog"><img src="../imgs/Settings-cog.png" /></div>
 
       <div class="table-group">
-        <div class="cups">
-          <img src="../imgs/white-cup.png" />
-          <img src="../imgs/white-cup.png" />
-          <img src="../imgs/white-cup.png" />
-          <img src="../imgs/white-cup.png" />
-          <img src="../imgs/white-cup.png" />
-          <img src="../imgs/white-cup.png" />
-          <img src="../imgs/white-cup.png" />
+        <div class="cup-container">
+          <img class="cup" draggable="true" src="../imgs/white-cup.png" />
+          <img class="cup" draggable="true" src="../imgs/white-cup.png" />
+          <img class="cup" draggable="true" src="../imgs/white-cup.png" />
+          <img class="cup" draggable="true" src="../imgs/white-cup.png" />
+          <img class="cup" draggable="true" src="../imgs/white-cup.png" />
+          <img class="cup" draggable="true" src="../imgs/white-cup.png" />
+          <img class="cup" draggable="true" src="../imgs/white-cup.png" />
         </div>
         <div class="table"><img src="../imgs/table.png" /></div>
       </div>
 
       <div class="play">Shuffling Cups...</div>`;
+
+    let iteration = 15;
+
+    //Recursive function that calls itself
+
+    function shuffleAndChangeCups() {
+      if (iteration <= 0) {
+        playGame();
+        return;
+      }
+
+      shuffle(cupColors);
+
+      console.log(cupColors);
+
+      const cups = document.querySelectorAll('.cup');
+      cups.forEach((cup, index) => {
+        cup.src = `../imgs/${cupColors[index]}-cup.png`;
+      });
+
+      iteration--;
+
+      setTimeout(shuffleAndChangeCups, 200);
+    }
+
+    shuffleAndChangeCups();
+
+    clicked = true;
+  }
 });
 
-let cupColors = [];
+function playGame() {
+  let counter = 0;
+
+  shuffle(comparisonColors);
+
+  for (let i = 0; i < cupColors.length; i++) {
+    if (cupColors[i] === comparisonColors[i]) {
+      console.log(cupColors[i]);
+      counter++;
+    }
+  }
+
+  console.log(`CupColors are: ${cupColors}`);
+  console.log(`comparisonColors are: ${comparisonColors}`);
+
+  document.querySelector('.play').innerHTML = `You have ${counter} correct.`;
+}
